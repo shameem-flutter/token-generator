4ed7b9746abd1034d04daac84a482cfb7792d05d
Aswin PP <126310448+Aswin-p-p@users.noreply.github.com>
2025-12-12 11:49:51 +0530

    chan

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite.myapp.apps.MyappConfig'
-',
+    'mysite.myapp.apps.MyappConfig',
 ]
 
 MIDDLEWARE = [
