60565a361fa421222c8e8264ec19a61d27d2ba14
Aswin PP <126310448+Aswin-p-p@users.noreply.github.com>
2025-12-12 12:37:07 +0530

    path('api/v1/myapp/', include("mysite.myapp.urls")),

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/myapp/',include("myapp.urls")),
+    path('api/v1/myapp/', include("mysite.myapp.urls")),
+    
 ]
